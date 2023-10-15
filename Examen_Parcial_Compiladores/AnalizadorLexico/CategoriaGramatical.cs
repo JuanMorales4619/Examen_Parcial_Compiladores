1f1987193b408ab7078a8271a13dd5184960ef44
JuanMorales4619 <juan.morales4619@uco.net.co>
2023-10-14 19:02:51 -0500

    Commit

---
+++ Examen_Parcial_Compiladores/AnalizadorLexico/CategoriaGramatical.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen_Parcial_Compiladores.AnalizadorLexico
+{
+    public enum CategoriaGramatical
+    {
+        LETRA_A, LETRA_B, LETRA_C, LETRA_D, LETRA_E, LETRA_F, LETRA_G, LETRA_H, LETRA_I, LETRA_J, LETRA_K, LETRA_L, LETRA_M, LETRA_N, LETRA_O, LETRA_P,
+        LETRA_Q, LETRA_R, LETRA_S, LETRA_T, LETRA_U, LETRA_V, LETRA_W, LETRA_X, LETRA_Y, LETRA_Z, LETRA_Ñ, LETRA_TILDADA_A, LETRA_TILDADA_E, LETRA_TILDADA_I, 
+        LETRA_TILDADA_O, LETRA_TILDADA_U, LETRA_DIERISIS_U, Digito_0, Digito_1, Digito_2, Digito_3, Digito_4, Digito_5, Digito_6, Digito_7, Digito_8, Digito_9, 
+        Coma, Punto_y_coma, Punto, Dos_puntos, Parentesis_abre, Parentesis_cierra, Corchetes_abre, Corchetes_cierra, Llaves_abre, Llaves_cierra, Numeral, Signo_peso,
+        SIGNO_AMPERSAN, SIGNO_ARROBA, SIGNO_MÁS, SIGNO_MENOS, SIGNO_MULTIPLICACION, SIGNO_DIVISION, PORCENTAJE, SIGNO_IGUAL, BARRA_INVERSA, PALO, COMILLA, 
+        ACENTO_CIRCUNFLEJO, EXCLAMACION_ABRE, EXCLAMACION_CIERRA, SIGNO_DE_PREGUNTA_ABRE, SIGNO_DE_PREGUNTA_CIERRA, GUION_BAJO, MAYOR_QUE, MENOR_QUE, 
+        SUPERINDICE_A, SUPERINDICE_0, VERGULILLA, COMILLA_ABRE, COMILLA_CIERRA, ESPACIO, COMILLA_SIMPLE, FIN_ARCHIVO, FIN_LINEA
+    }
+}
--- Examen_Parcial_Compiladores/Form1.Designer.cs
+++ Examen_Parcial_Compiladores/Form1.Designer.cs
@@ -38,6 +38,11 @@
             this.label3 = new System.Windows.Forms.Label();
             this.btnCompilar = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
             this.SuspendLayout();
             // 
             // comboBox1
@@ -47,7 +52,7 @@
             "",
             "Cargar desde archivo",
             "Insertar texto"});
-            this.comboBox1.Location = new System.Drawing.Point(108, 79);
+            this.comboBox1.Location = new System.Drawing.Point(21, 28);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(255, 21);
             this.comboBox1.TabIndex = 0;
@@ -64,7 +69,7 @@
             "Puntos a numeros",
             "Numeros a texto",
             "Numeros a punto"});
-            this.comboBox2.Location = new System.Drawing.Point(529, 79);
+            this.comboBox2.Location = new System.Drawing.Point(442, 28);
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Size = new System.Drawing.Size(277, 21);
             this.comboBox2.TabIndex = 1;
@@ -72,7 +77,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(105, 54);
+            this.label1.Location = new System.Drawing.Point(18, 3);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(86, 13);
             this.label1.TabIndex = 2;
@@ -82,7 +87,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(526, 54);
+            this.label2.Location = new System.Drawing.Point(439, 3);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(105, 13);
             this.label2.TabIndex = 3;
@@ -91,7 +96,7 @@
             // EntradaManual
             // 
             this.EntradaManual.Enabled = false;
-            this.EntradaManual.Location = new System.Drawing.Point(108, 182);
+            this.EntradaManual.Location = new System.Drawing.Point(21, 131);
             this.EntradaManual.Multiline = true;
             this.EntradaManual.Name = "EntradaManual";
             this.EntradaManual.Size = new System.Drawing.Size(255, 182);
@@ -100,7 +105,7 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(529, 182);
+            this.textBox2.Location = new System.Drawing.Point(442, 131);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
             this.textBox2.ReadOnly = true;
@@ -110,7 +115,7 @@
             // btnCargar1
             // 
             this.btnCargar1.Enabled = false;
-            this.btnCargar1.Location = new System.Drawing.Point(180, 243);
+            this.btnCargar1.Location = new System.Drawing.Point(93, 192);
             this.btnCargar1.Name = "btnCargar1";
             this.btnCargar1.Size = new System.Drawing.Size(116, 51);
             this.btnCargar1.TabIndex = 6;
@@ -123,7 +128,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Enabled = false;
-            this.label3.Location = new System.Drawing.Point(177, 318);
+            this.label3.Location = new System.Drawing.Point(90, 267);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(35, 13);
             this.label3.TabIndex = 7;
@@ -132,7 +137,7 @@
             // 
             // btnCompilar
             // 
-            this.btnCompilar.Location = new System.Drawing.Point(406, 243);
+            this.btnCompilar.Location = new System.Drawing.Point(319, 192);
             this.btnCompilar.Name = "btnCompilar";
             this.btnCompilar.Size = new System.Drawing.Size(89, 48);
             this.btnCompilar.TabIndex = 8;
@@ -143,33 +148,66 @@
             // textBox1
             // 
             this.textBox1.Enabled = false;
-            this.textBox1.Location = new System.Drawing.Point(108, 370);
+            this.textBox1.Location = new System.Drawing.Point(21, 319);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(255, 182);
             this.textBox1.TabIndex = 9;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(31, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(840, 558);
+            this.tabControl1.TabIndex = 10;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(832, 532);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.comboBox1);
+            this.tabPage1.Controls.Add(this.btnCompilar);
+            this.tabPage1.Controls.Add(this.comboBox2);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.btnCargar1);
+            this.tabPage1.Controls.Add(this.EntradaManual);
+            this.tabPage1.Controls.Add(this.textBox2);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(832, 532);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(957, 605);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.btnCompilar);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.btnCargar1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.EntradaManual);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.comboBox2);
-            this.Controls.Add(this.comboBox1);
+            this.ClientSize = new System.Drawing.Size(885, 582);
+            this.Controls.Add(this.tabControl1);
             this.Name = "Form1";
             this.Text = "Compilador de clase";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -185,6 +223,9 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button btnCompilar;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
     }
 }
 
